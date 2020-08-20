09476de3c180ba81198d9b612fe863be275ceffb
Geoff <geoff.harper@industrialskyworks.com>
2020-08-20 10:40:14 -0400

    render obj

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React, { useEffect } from "react";
 import "./App.css";
+import { Scene, PerspectiveCamera, WebGLRenderer } from "three";
 import { OBJLoader2Parallel } from "three/examples/jsm/loaders/OBJLoader2Parallel";
 
 function App() {
   useEffect(() => {
     (async () => {
-      await new Promise((res) =>
-        new OBJLoader2Parallel().load(`http://localhost:8080/model.obj`, (model) =>
-          res(model)
+      const model = await new Promise((res) =>
+        new OBJLoader2Parallel().load(
+          `http://localhost:8080/model.obj`,
+          (model) => res(model)
         )
       );
+
+      const scene = new Scene();
+      const camera = new PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+      );
+
+      const renderer = new WebGLRenderer();
+      renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
+      document.body.appendChild(renderer.domElement);
+
+      scene.add(model);
+      camera.position.z = 5;
+
+      const animate = () => {
+        requestAnimationFrame(animate);
+        renderer.render(scene, camera);
+      };
+      animate();
     })();
   }, []);
 
